c7e826fc5e9b50cb8365e9221d56bf6d5b447a78
DanielDarwiche <danielinti1@hotmail.com>
2023-10-30 15:27:45 +0100

    style

--- script.js
+++ script.js
@@ -11,27 +11,27 @@ breathAnimation();
 
 function breathAnimation() {
     text.innerText = 'Breathe in';
-    text.style.color = 'white'; // Correct the color format by adding a '#' before the hex code
+    text.style.textShadow = '1px 1px 5px yellow';
+    text.style.color = 'white';
     container.className = 'container grow';
 
     setTimeout(() => {
         text.innerText = 'Hold';
-        text.style.color = 'orange'; // Correct the color format by adding a '#' before the hex code
+        text.style.color = 'orange';
         container.className = 'container grow';
 
         setTimeout(() => {
             text.innerText = 'Breathe out';
-            text.style.color = 'white'; // Correct the color format by adding a '#' before the hex code
-            text.style.textShadow = '1px 1px 5px lightblue'; // Use correct property name 'textShadow'
+            text.style.color = 'white';
+            text.style.textShadow = '1px 1px 5px blue';
             container.className = 'container grow';
             container.className = 'container shrink';
         }, holdTime);
     }, breatheTime);
 }
 
-setInterval(breathAnimation, totalTime);//run it every seven seconds
+setInterval(breathAnimation, totalTime);
 
 //skall ändra till hold efter breathe out
 //skall ändra till lite längre secunder
-//lite mindre cirkel => för mobilen ju!
 //input i hur många sekunder? 
